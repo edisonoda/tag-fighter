5b2212ae8e41fc4dab94ca941748d1392748bb6a
Edison Furusato <edison.oda@hotmail.com>
2025-11-27 11:11:22 -0300

    Implemented pause, fixed menu toggle and adjusted player default sprite

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,28 @@
+import { Game } from "./game.js";
+
 const menu = document.getElementById('main-menu');
 const startBtn = document.getElementById('start');
 const settingsBtn = document.getElementById('settings');
 const creditsBtn = document.getElementById('credits');
 
-const main = document.getElementById('main');
-
-menu.showModal();
 document.addEventListener('keyup', key => {
-    if (key.code === 'Escape')
-        menu.showModal();
+    if (key.code === 'Escape') {
+        if (menu.open)
+            menu.close();
+        else
+            menu.showModal();
+    }
+});
+
+menu.addEventListener('keydown', ev => {
+    ev.preventDefault();
+});
+
+menu.addEventListener('toggle', ev => {
+    if (ev.newState === 'closed')
+        Game.paused = false;
+    else
+        Game.paused = true;
 });
 
 startBtn.addEventListener('click', () => {
@@ -22,3 +36,5 @@ settingsBtn.addEventListener('click', () => {
 creditsBtn.addEventListener('click', () => {
     
 });
+
+menu.showModal();
